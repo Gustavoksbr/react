a68d33c1dee80f4263d046972c8b965cebb38e0f
gustavoksbr <gustavosalesi@hotmail.com>
2024-04-28 18:50:50 -0300

    atualizacao

--- src/index.js
+++ src/index.js
@@ -3,15 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Teste from './componente/teste.js';
+import Letreiro from './componente/Letreiro.js';
+import Relogio from './componente/Relogio.js';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <>
   <React.StrictMode>
     <App />
+    <Teste/>
+    <Letreiro/>
+    <Relogio/>
   </React.StrictMode>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
+
+
+
+
