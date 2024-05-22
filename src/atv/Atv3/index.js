c40c6a009a9aca6921aef121b2db8fe9f6cb5a6e
Gustavoksbr <151581349+Gustavoksbr@users.noreply.github.com>
2024-05-21 21:36:06 -0300

    Atv3 Loop

--- src/atv/Atv3/index.js
+++ src/atv/Atv3/index.js
@@ -1,28 +1,26 @@
 
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import '../CSS/index.css';
 
 
-export default function Atv3()
-{
-    return(
+
+export default function Atv3() {
+
+    const lista = ["Toolbar", "Gallery", "Counter", "MovingDot", "ShapeEditor", "Form", "Form2", "Form3", "List", "List2", "List3", "List4", "CounterList", "BucketList"];
+
+    return (
         <div className='Principal'>
             <h1>Atividade 3</h1>
-            <Link to="/Atv3/Toolbar_"> Toolbar</Link>
-            <Link to="/Atv3/Gallery_"> Gallery</Link>
-            <Link to="/Atv3/Counter_"> Counter</Link>
-            <Link to="/Atv3/Form_"> Form</Link>
-            <Link to="/Atv3/MovingDot_"> MovingDot</Link>
-            <Link to="/Atv3/Form2_"> Form2</Link>
-            <Link to="/Atv3/Form3_"> Form3</Link>
-            <Link to="/Atv3/List_"> List</Link>
-            <Link to="/Atv3/List2_"> List2</Link>
-            <Link to="/Atv3/ShapeEditor_"> ShapeEditor</Link>
-            <Link to="/Atv3/CounterList_"> CounterList</Link>
-            <Link to="/Atv3/List3_"> List3</Link>
-            <Link to="/Atv3/List4_"> List4</Link>
-            <Link to="/Atv3/BucketList_"> BucketList</Link>
+            <ul>
+
+            
+        {lista.map((elemento) => (
+          <li key={elemento.id}><Link to = {'/Atv3/' + elemento+'_'}>{elemento}</Link></li>
+        ))}
+            </ul>
+
+
             <Link to="/"> Retornar a página inicial</Link>
         </div>
     );
-}
+        }
